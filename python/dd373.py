bc0732798102b241caab3f7286b1eb5c887fe106
huangj <huangj@yuzhi.cn>
2020-08-26 09:45:24 +0800

    dev

--- python/dd373.py
+++ python/dd373.py
@@ -8,6 +8,9 @@ import traceback
 import sys
 import zlib
 import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 # reload(sys)
 # sys.setdefaultencoding('utf-8')
@@ -15,6 +18,9 @@ logging.basicConfig(level=logging.INFO,format='%(asctime)s|%(levelname)s|%(proce
 
 is_from_local = True
 # is_from_local = False
+
+toggle_price1 = 0.05
+
 # 
 def curl_get(url, timeout=5, proxy=False, headers=None, gzip=False):
     if headers is None:
@@ -73,8 +79,13 @@ def handle_single_page(url):
     reg = r'(?<=<p class="font12 color666 m-t5">1金\=).*(?=元</p>)';
     rr = re.compile(reg, re.M|re.I)
     r = rr.findall(html)
-    r.sort()
-    print(r)
+    r2 = []
+    for x in r:
+        r2.append(float(x))
+    r2.sort()
+    if r2[0] < toggle_price1:
+        send_mail()
+    logging.info(r2)
 
 
 def run():
@@ -86,6 +97,35 @@ def run():
 def run2():
     print(range(1, 20))
 
+def send_mail():
+    my_sender='hjshiwbd@163.com'# 发件人邮箱账号
+    my_pass = 'EJZSGXNJGAVHMUAI'# 发件人邮箱密码
+    my_user='34489659@qq.com'# 收件人邮箱账号，我这边发送给自己
+    smtp_server='smtp.163.com'
+    def mail():
+        ret=True
+        try:
+            msg=MIMEText('cheaper found','plain','utf-8')
+            msg['From']=formataddr(["gold spider",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+            msg['To']=formataddr(["hjin",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+            msg['Subject']="cheaper found"                # 邮件的主题，也可以说是标题
+     
+            server=smtplib.SMTP_SSL(smtp_server, 465)  # 发件人邮箱中的SMTP服务器，端口是25
+            # server.set_debuglevel(1)
+            server.helo(smtp_server)
+            server.ehlo(smtp_server)
+            server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+            server.sendmail(my_sender,[my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+            server.quit()  # 关闭连接
+        except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+            traceback.print_exc()
+            ret=False
+        return ret
+    ret=mail()
+    if ret:
+        print("send mail success")
+    else:
+        print("send mail fail")
 
 if __name__ == '__main__':
     run()
