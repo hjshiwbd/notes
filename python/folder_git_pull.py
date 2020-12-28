8beaf4d47989c8383afdee11bc4978b9f0fc4e60
huangj <huangj@yuzhi.cn>
2020-12-28 10:21:35 +0800

    folder_git_pull.py

---
+++ python/folder_git_pull.py
@@ -0,0 +1,79 @@
+import sys
+import os
+
+"""
+对root文件夹下所有git仓库执行git pull
+约定:
+  默认root下每个文件夹都是一个仓库,执行git pull
+  root下以"!"开头的文件夹表示下一个仓库的多个分支,会对每个仓库都git pull
+  全局变量excludes表示不是仓库的文件夹,不处理
+  
+eg:
+  root=D:\\git\\yuzhi
+  root下有子文件夹"wechat-parent", "!admin", "!new folder"
+  "wechat-parent"是普通仓库,
+  "!admin"是包含多个分支的仓库,目录结构为:D:\\git\\yuzhi\\!admin\\1.2.0\\yuzhi-app-admin (root\\二级仓库简称\\三级分支版本名\\四级仓库名)
+  "!new folder"是不处理的文件夹
+"""
+
+# 指定的不pull
+excludes = ["!new folder"]
+# !开头的要搜索其二级目录,并pull
+# !开头的!new folder不pull
+
+root = sys.path[0]
+
+
+def is_exclude(name):
+    flag1 = name in excludes
+    return flag1
+
+
+def get_folers(root_folder):
+    result = []
+    a = os.listdir(root_folder)
+    for file_name in a:
+        if is_exclude(file_name):
+            continue
+        p = os.path.join(root_folder, file_name)
+        if not os.path.isfile(p):
+            # if not is_exclude(file_name):
+            if file_name.startswith("!"):
+                # !开头的文件夹,其下每个文件夹都是一个仓库
+                # D:\git\yuzhi\!admin\4.0.0\yuzhi-app-admin
+                lv2_folder = os.path.join(root_folder, file_name)
+                lv3_name_list = os.listdir(lv2_folder)
+                for lv3_name in lv3_name_list:
+                    lv3_folder = os.path.join(lv2_folder, lv3_name)
+                    lv4_name = os.listdir(lv3_folder)[0]
+                    result.append(os.path.join(lv3_folder, lv4_name))
+            else:
+                p = os.path.join(root_folder, file_name)
+                result.append(p)
+    return result
+
+
+def list_to_bat(list):
+    total = len(list)
+    count = 0
+    result = []
+    for p in list:
+        count = count + 1
+        result.append("echo %s, %s/%s" % (p, str(count), str(total)))
+        result.append("git -C \"" + p + "\" pull")
+    result.append("pause")
+    txt = "\n".join([x for x in result])
+    return txt
+
+
+def run():
+    list1 = get_folers(root)
+    txt = list_to_bat(list1)
+    # print(txt)
+    io = open('pull.bat', "w+")
+    io.write(txt)
+    io.close()
+
+
+if __name__ == '__main__':
+    run()
--- python/wcl/python/reportaward.py
+++ python/wcl/python/reportaward.py
@@ -1,10 +1,6 @@
 #  -*- coding:utf-8 -*-  
 import logging
 import utils
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s|%(levelname)s|%(process)d|%(filename)s.%(lineno)d|%(message)s',
@@ -26,4 +22,6 @@ select t.name,t.class,t1.player_amount from wcl_player  t join
 (select  * from wcl_report_player  where report_id =428 and report_type = 'dmg') t1 on t.`name` = t1.player_name
 where t.class = 'rogue'
 """
+        with utils.connect_by_code('test001') as conn:
+            return utils.query_list(conn, sql)
         pass
