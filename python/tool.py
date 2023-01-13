b0c0e92f841e1981fec108a9c5fa49d63865cf16
huangj <huangj@yuzhi.cn>
2023-01-13 18:01:17 +0800

    dev

---
+++ python/tool.py
@@ -0,0 +1,55 @@
+import urllib
+import urllib.request
+import zlib
+
+import redis
+
+# master001.yz的值
+# redis_host
+# redis_port
+# redis_password
+# redis_db
+# redis_w_key = "w:token:exchange_rate"
+
+
+def redis_conn_master001():
+    """
+    wowtoken.py
+    """
+    r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=redis_db, decode_responses=True)
+    return r
+
+
+def get_token_rate():
+    """
+    dd373.py
+    """
+    with redis_conn_master001() as r:
+        return float(r.get(redis_w_key))
+
+
+def curl_get(url, timeout=5, proxy=False, headers=None, gzip=False):
+    """
+    wowtoken.py
+    dd373.py
+    crawler_515fa.py
+    crawler_amac.py
+    crawler_for_some_site.py
+    """
+    if headers is None:
+        headers = {}
+    opener = urllib.request.build_opener()
+    if proxy:
+        proxy_info = {'host': '127.0.0.1',
+                      'port': 7890}
+        proxy_support = urllib.ProxyHandler(
+            {"http": "http://%(host)s:%(port)d" % proxy_info})
+        opener = urllib.build_opener(proxy_support)
+
+    request = urllib.request.Request(url, headers=headers)
+
+    resp = opener.open(request, timeout=timeout)
+    resp_html = resp.read()
+    if gzip:
+        resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS)
+    return resp_html
