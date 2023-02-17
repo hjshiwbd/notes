e2c36ecf6557beebda694d2736d0eeb969f420d1
huangj <huangj@yuzhi.cn>
2023-02-17 17:29:23 +0800

    dev

--- python/novel_crawler/novel_crawler.py
+++ python/novel_crawler/novel_crawler.py
@@ -16,17 +16,17 @@ import logging
 import utils
 from lxml import etree
 
-novel_list_url = 'https://www.bixiawenku.com/41_41366/'
-novel_site_encoding = 'gbk'
-xpath_novel_name = '//div[@class="book-describe"]/h1/text()'
-xpath_novel_author = '//div[@class="book-describe"]/p[1]/text()'
+novel_list_url = 'https://www.285b.com/index/49910'
+novel_site_encoding = 'utf-8'
+xpath_novel_name = '//div[@class="atitle"]/text()'
+xpath_novel_author = '//div[@class="ainfo"]/a/text()'
 
-xpath_chapter_list = '//div[@class="book-list clearfix"]//a/@href'
+xpath_chapter_url = '//dl[@class="index"]/dd/a/@href'
 
-xpath_chapter_title = '//h1[@id="nr_title"]/text()'
-xpath_chapter_content = '//div[@id="nr1"]/text()'
+xpath_chapter_title = '//div[@class="atitle"]/text()'
+xpath_chapter_content = '//div[@id="acontent"]/text()'
 
-from_remote = False
+from_remote = True
 
 
 def xpath_one(html, xpath):
@@ -55,7 +55,7 @@ def run():
 
     logging.info(f'bookname: {bookname}, bookauthor: {bookauthor}')
 
-    chapter_url_list = html.xpath(xpath_chapter_list)
+    chapter_url_list = html.xpath(xpath_chapter_url)
 
     output_str = ''
     count = 0
--- python/novel_crawler/novel_crawler_cst.py
+++ python/novel_crawler/novel_crawler_cst.py
@@ -11,6 +11,15 @@ xpath_chapter_title = '//h1[@id="nr_title"]/text()'
 xpath_chapter_content = '//div[@id="nr1"]/text()'
 
 -------------------------------
+novel_list_url = 'https://www.285b.com/index/49910'
+novel_site_encoding = 'utf-8'
+xpath_novel_name = '//div[@class="atitle"]/text()'
+xpath_novel_author = '//div[@class="ainfo"]/a/text()'
+
+xpath_chapter_url = '//dl[@class="index"]/dd/a/@href'
+
+xpath_chapter_title = '//div[@class="atitle"]/text()'
+xpath_chapter_content = '//div[@id="acontent"]/text()'
 
 -------------------------------
 
