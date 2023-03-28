a2ff5b49b2922bde22abe6b62f2af93a2a3308e7
huangj <huangj@yuzhi.cn>
2023-03-28 21:30:22 +0800

    dev

--- win-bat/mysql-backup-windnows.bat
+++ win-bat/mysql-backup-windnows.bat
@@ -3,6 +3,8 @@
 if %time:~0,2% leq 9 (set hour=0%time:~1,1%) else (set hour=%time:~0,2%) 
 set dd=%date:~0,4%%date:~5,2%%date:~8,2%_%hour%%time:~3,2%%time:~6,2%
 
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" --lock-tables=false -u root -P 3306 -proot crawler | gzip  > e:\local_mysql_bak\crawler_%dd%.sql.gz
+"D:\mysql-8.0.32-winx64\bin\mysqldump.exe" --default-character-set=utf8 --lock-tables=false -u root -P 3306 -p crawler  | gzip  > f:\local_mysql_bak\crawler_%dd%.sql.gz
 
-echo bak file: e:\local_mysql_bak\crawler_%dd%.sql.gz
+echo bak file: f:\local_mysql_bak\crawler_%dd%.sql.gz
+
+pause
