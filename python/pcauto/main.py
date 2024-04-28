cced5aaad7454adeb4a9b5c3625b2d747223cc86
huangj <huangj@yuzhi.cn>
2024-04-28 17:06:51 +0800

    dev

--- python/pcauto/main.py
+++ python/pcauto/main.py
@@ -58,8 +58,8 @@ mysql_pass = 'hH01KgJMPbVJcYbNAzp@oVe9DdbL4Usg'
 
 site_domain = 'https://price.pcauto.com.cn'
 
-start = '2024-02'
-end = '2024-02'
+start = '2024-03'
+end = '2024-03'
 
 # vehcle_type = {
 #     "car": "1",
